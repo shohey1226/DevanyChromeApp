719968fb53adae04bf93f523ecd66a28a64f22c9
shohey1226 <shoheik@cpan.org>
2018-09-04 00:34:42 +0000

    removed default window size

--- background.js
+++ background.js
@@ -1,8 +1,4 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
-    'outerBounds': {
-      'width': 1200,
-      'height':1000 
-    }
   });
 });
